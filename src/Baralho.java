52942f887ca64398c9a4204f40c5df4e04842e13
Gusta <gustavofrsnt@gmail.com>
2023-07-29 12:47:36 -0300

    Change csvs columns

--- src/Baralho.java
+++ src/Baralho.java
@@ -4,10 +4,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Baralho extends LinkedList<Carta>{
     private String tema;
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public void adicionarCarta(Carta carta) {
+        this.add(carta);
+    }
 
     public Baralho(String tema) {
         this.tema = tema;
@@ -21,18 +33,23 @@ public class Baralho extends LinkedList<Carta>{
         String[] nomeAtributos = null;
         String[] nomeCartas = null;
         String[] codigo = null;
-        int[][] valorAtributosTabela = null;
+        String[] descricao = null;
+        Boolean[] isSuperTrunfo = null;
+        String[][] valorAtributosTabela = null;
         try {
             reader = new BufferedReader(new FileReader(arquivo));
-            String[][] tabela = new String[33][6];
+            String[][] tabela = new String[34][9];
             int i = 0;
             while((linha = reader.readLine()) != null) {
-                String[] palavrasLinha = linha.split(",");
+                String[] palavrasLinha = linha.split(";");
                 int j = 0;
+                //System.out.printf("%d ", i);
                 for(String palavra : palavrasLinha) {
                     tabela[i][j] = palavra;
+                    //System.out.printf("%s ; ",palavra);
                     j++;
                 }
+                //System.out.println();
                 i++;
             }
 
@@ -40,15 +57,20 @@ public class Baralho extends LinkedList<Carta>{
             nomeAtributos = new String[4];
             for(i = 0; i < 4; i++) {
                 nomeAtributos[i] = tabela[0][i+1];
+                //System.out.println(nomeAtributos[i]);
             }
             nomeCartas = new String[32];
             codigo = new String[32];
-            valorAtributosTabela = new int[32][4];
+            isSuperTrunfo = new Boolean[32];
+            descricao = new String[32];
+            valorAtributosTabela = new String[32][4];
             for(i = 0; i < 32; i++) {
                 nomeCartas[i] = tabela[i+1][0];
                 codigo[i] = tabela[i+1][5];
+                isSuperTrunfo[i] = Boolean.parseBoolean(tabela[i+1][6]);
+                descricao[i] = tabela[i+1][7];
                 for(int j = 0; j < 4; j++) {
-                    valorAtributosTabela[i][j] = Integer.parseInt(tabela[i+1][j+1]);
+                    valorAtributosTabela[i][j] = tabela[i+1][j+1];
                 }
             }
         }catch(Exception e) {
@@ -63,11 +85,11 @@ public class Baralho extends LinkedList<Carta>{
         }
 
         for(int i = 0; i < 32; i++) {
-            int[] valorAtributos = new int[4];
+            String[] valorAtributos = new String[4];
             for(int j = 0; j < 4; j++) {
                 valorAtributos[j] = valorAtributosTabela[i][j];
             }
-            Carta carta = new Carta(nomeAtributos, valorAtributos, nomeCartas[i], codigo[i]);
+            Carta carta = new Carta(nomeAtributos, valorAtributos, nomeCartas[i], codigo[i], isSuperTrunfo[i], descricao[i]);
             this.add(carta);
         }
     }
@@ -75,20 +97,20 @@ public class Baralho extends LinkedList<Carta>{
     public void embaralhar() {
         if(!this.isEmpty()){
             Collections.shuffle(this);
+            Collections.shuffle(this);
         }else{
-            System.out.println("Este baralho está vazio! Use o método carregar() antes de poder embaralhar.");
+            System.out.println("Este baralho está vazio!");
         }
     }
 
     public void distribuir(JogadorAbstrato jogador1, JogadorAbstrato jogador2) {
         Iterator<Carta> iterator = this.iterator();
-        Random random = new Random();
 
         JogadorAbstrato[] jogadores = new JogadorAbstrato[2];
         jogadores[0] = jogador1;
         jogadores[1] = jogador2;
 
-        JogadorAbstrato toggleJogador = jogadores[random.nextInt((1 - 0) + 1) + 0];
+        JogadorAbstrato toggleJogador = jogadores[ThreadLocalRandom.current().nextInt(0, 1 + 1)];
 
         while(iterator.hasNext()) {
 
--- src/Principal.java
+++ src/Principal.java
@@ -6,9 +6,9 @@ public class Principal {
 
         System.out.println("============== Super Trunfo ==============");
         System.out.println("Escolha um tema para o jogo: ");
-        System.out.println("\tBen 10\n\tOne Piece\n\tBNHA");
+        System.out.println("\tBen 10\n\tOne Piece\n\tBNHA\n\tDeuses");
         String tema = scanner.nextLine();
-        
         Jogo jogo = new Jogo(tema);
+        jogo.jogar();
     }
 }
