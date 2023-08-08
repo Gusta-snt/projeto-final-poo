9ff28d78c718ea2fce3d4a9dd7d0fce6edb1ce29
Gusta <gustavofrsnt@gmail.com>
2023-08-08 16:59:10 -0300

    Change Carta rule

--- src/Carta.java
+++ src/Carta.java
@@ -1,15 +1,17 @@
 public class Carta {
 
   private Atributo[] atributos = new Atributo[4];
-
   private String nome;
+  private String codigo;
+  private String isSuperTrunfo;
+  private String descricao;
 
-  public Carta( String[] nomeAtributos, Atributo atributos, String[] nomeAtributos,int[] valorAtributos ) {
+  public Carta(String[][] nomeUnidadeAtributos, int[] valorAtributos, String nome, String codigo, boolean isSuperTrunfo, String descricao) {
     this.nome = nome;
-    atributos[0] = new Atributo(nomeAtributo[0], valorAtributos[0]);
-    atributos[1] = new Atributos(nomeAtributos[1], valorAtributos[1]);
-    atributos[2] = new Atributos(nomeAtributos[2], valorAtributos[2]);
-    atributos[3] = new Atributos(nomeAtributos[3], valorAtributos[3]);
+
+    for(int i = 0; i < 4; i++) {
+      atributos[i] = new Atributo(nomeUnidadeAtributos[i][0], valorAtributos[i], nomeUnidadeAtributos[i][1]);
+    }
   }
 
   public String getNome() {
@@ -17,6 +19,44 @@ public class Carta {
   }
 
   public void setNome(String nome) {
-    return nome;
+    this.nome = nome;
+  }
+
+  public Atributo[] getAtributos() {
+    return atributos;
+  }
+
+  public void setAtributos(Atributo[] atributos) {
+    this.atributos = atributos;
+  }
+
+  public String getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(String codigo) {
+    this.codigo = codigo;
+  }
+
+  public String getIsSuperTrunfo() {
+    return isSuperTrunfo;
+  }
+
+  public void setIsSuperTrunfo(String isSuperTrunfo) {
+    this.isSuperTrunfo = isSuperTrunfo;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  @Override
+  public String toString() {
+    return "Carta [atributos=" + atributos.toString() + ", nome=" + nome + ", codigo=" + codigo
+        + ", isSuperTrunfo=" + isSuperTrunfo + ", descricao=" + descricao + "]";
   }
 }
